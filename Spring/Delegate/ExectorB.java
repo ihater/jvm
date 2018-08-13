9d335b0b79e958940f1c371cee8898d66b0db2be
ihater <1043182147@qq.com>
2018-08-13 20:54:26 +0800

    委派模式
    两个员工类，被经理类调用执行
    看似是经理执行的任务，其实最终还是员工在执行
    经理委派给员工

---
+++ Spring/Delegate/ExectorB.java
@@ -0,0 +1,14 @@
+package Delegate;
+
+/**
+ *	执行者B
+ */
+public class ExectorB implements IExector{
+
+	@Override
+	public void doing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Spring/Delegate/IExector.java
@@ -0,0 +1,10 @@
+package Delegate;
+/**
+ *	架构师定义的  ,要执行的任务
+ */
+public interface IExector {
+
+	// 架构师定义的  执行
+	// 在公司中，员工执行的任务，这里有doing，公司里有员工A 跟员工B来实现
+	void doing();
+}
